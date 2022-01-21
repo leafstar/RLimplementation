bug fixed. note that we have to count actions separately, otherwise the average reward is not what we want.

bandit2.1.py
@@ -11,6 +11,7 @@ def bandit(mu, sigma, k, t, epsilon):
     q = np.random.normal(mu, sigma, k)  # generate true q's
     a_best = np.argmax(q)
     Q = np.zeros(k)  # initialize Q vector
+    N = np.zeros(k)  # initialize N, a vector contains how many times an action has been taken
     Qlist = []  # the vector for storing the rewards
     for i in range(t):
         u = np.random.uniform(0, 1, 1)  # sample from uniform distribution
@@ -18,10 +19,10 @@ def bandit(mu, sigma, k, t, epsilon):
             a = np.random.choice(10, 1)
         else:
             a = np.argmax(Q)
-        a = np.argmax(Q)
         R = np.random.normal(q[a], 1, 1)  # reward
-        Qlist.append(R)  # get reward
-        Q[a] += 1 / (i + 1) * (R - Q[a])
+        N[a] += 1
+        Q[a] += 1 / N[a] * (R - Q[a])
+        Qlist.append(Q[a])  # get reward
     return q, Q, Qlist
 
 
@@ -35,10 +36,11 @@ def run(n,epsilon):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    y1 = run(2000, 0)
+
     y2 = run(2000, 0.01)
+    y1 = run(2000, 0)
     y3 = run(2000, 0.1)
-    plt.plot(range(1000), y1)
-    plt.plot(range(1000), y2)
-    plt.plot(range(1000), y3)
+    plt.plot(range(1000), y1, 'g')
+    plt.plot(range(1000), y2, 'r')
+    plt.plot(range(1000), y3, 'b')
     plt.show()